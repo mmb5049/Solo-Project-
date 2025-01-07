428eacc1846a42c88a79d270d9b404ae0772eb32
mmb5049 <mmb5049@g.rit.edu>
2025-01-06 22:18:37 -0500

    Finish some movement

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Rigidbody2D rb;
+    public float movementSpeed = 10f;
+    public float jumpForce = 30f;
+    public Vector2 moveDirection = new Vector2(0, 1);
+
     void Start()
     {
         
@@ -14,6 +20,34 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    void FixedUpdate()
+    {
+        // Continuous movement based on input
+        rb.velocity = new Vector2(moveDirection.x * movementSpeed, rb.velocity.y);
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            Debug.Log("jump");
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed || context.phase == InputActionPhase.Started)
+        {
+            // Update moveDirection when input is performed
+            moveDirection = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            // Stop movement when input is canceled
+            moveDirection = Vector2.zero;
+        }
     }
 }
